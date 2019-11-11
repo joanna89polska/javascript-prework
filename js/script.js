8e6eab18884b92ba8dfb041c1bf36d740700e6c7
Joanna Polska <joanna19polska@gmail.com>
2019-11-11 04:23:13 +0100

    new options

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-let computerMove = `kamień`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = `papier`;
+let computerMove = 'nieznany ruch';
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+  computerMove = 'papier';
+}
+else(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch to: ' + computerMove);
